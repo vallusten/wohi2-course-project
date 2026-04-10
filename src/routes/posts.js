513dca0f5b841c2deb291e253c4fa962d52c21e5
vallusten <valhermo@uef.fi>
2026-04-10 14:28:58 +0300

    Fixed posts API

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -6,17 +6,7 @@ const posts = require("../data/posts");
 // GET /posts 
 // List all posts
 router.get("/", (req, res) => {
-  const { keyword } = req.query;
-
-  if (!keyword) {
-    return res.json(posts);
-  }
-
-  const filteredPosts = posts.filter(post =>
-    post.keywords.includes(keyword.toLowerCase())
-  );
-
-  res.json(filteredPosts);
+  res.json(posts);
 });
 // GET /posts/:postId
 // Show a specific post
@@ -34,18 +24,18 @@ router.get("/:postId", (req, res) => {
 // POST /posts
 // Create a new post
 router.post("/", (req, res) => {
-  const { title, date, content, keywords } = req.body;
+  const { question, answer } = req.body;
 
-  if (!title || !date || !content) {
+  if (!question || !answer) {
     return res.status(400).json({
-      message: "title, date, and content are required"
+      message: "Question and answer are required"
     });
   }
   const maxId = Math.max(...posts.map(p => p.id), 0);
 
   const newPost = {
     id: posts.length ? maxId + 1 : 1,
-    title, date, content,
+    question, answer,
     keywords: Array.isArray(keywords) ? keywords : []
   };
   posts.push(newPost);
@@ -55,7 +45,7 @@ router.post("/", (req, res) => {
 // Edit a post
 router.put("/:postId", (req, res) => {
   const postId = Number(req.params.postId);
-  const { title, date, content, keywords } = req.body;
+  const { question, answer } = req.body;
 
   const post = posts.find((p) => p.id === postId);
 
@@ -63,15 +53,14 @@ router.put("/:postId", (req, res) => {
     return res.status(404).json({ message: "Post not found" });
   }
 
-  if (!title || !date || !content) {
+  if (!question || !answer) {
     return res.json({
-      message: "title, date, and content are required"
+      message: "Question and answer are required"
     });
   }
 
-  post.title = title;
-  post.date = date;
-  post.content = content;
+  post.question = question;
+  post.answer = answer;
   post.keywords = Array.isArray(keywords) ? keywords : [];
 
   res.json(post);
