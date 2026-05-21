a615b1776d1d3884056a82582278f2d16e4c71be
Valtteri Hermonen <164264621+vallusten@users.noreply.github.com>
2026-05-21 16:44:49 +0300

    Bugfixing in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const app = require("./app");
+/*const app = require("./app");
 const logger = require("./lib/logger");
 const prisma = require("./lib/prisma");
 
@@ -12,4 +12,31 @@ async function shutdown() {
   server.close(() => process.exit(0));
 }
 process.on("SIGINT",  shutdown);
+process.on("SIGTERM", shutdown); */
+console.log("1 starting");
+
+const app = require("./app");
+console.log("2 app loaded");
+
+const logger = require("./lib/logger");
+console.log("3 logger loaded");
+
+const prisma = require("./lib/prisma");
+console.log("4 prisma loaded");
+
+const PORT = process.env.PORT || 3000;
+console.log("5 starting server");
+
+const server = app.listen(PORT, "0.0.0.0", () => {
+  console.log("6 server listening");
+  logger.info({ port: PORT }, "server listening");
+});
+
+async function shutdown() {
+  console.log("7 shutting down");
+  await prisma.$disconnect();
+  server.close(() => process.exit(0));
+}
+
+process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
