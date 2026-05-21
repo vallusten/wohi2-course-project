c21950bf2d5d3973911200eac0cd6751dffbb1a9
Valtteri Hermonen <164264621+vallusten@users.noreply.github.com>
2026-05-21 16:31:14 +0300

    Revert index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const logger = require("./lib/logger");
 const prisma = require("./lib/prisma");
 
 const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   logger.info({ port: PORT }, "server listening");
 });
 
