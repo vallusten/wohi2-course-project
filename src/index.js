492d52ac7e4162721e81a346aeecf61fff0e1cf9
Valtteri Hermonen <164264621+vallusten@users.noreply.github.com>
2026-05-21 17:05:46 +0300

    Cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
-/*const app = require("./app");
-const logger = require("./lib/logger");
-const prisma = require("./lib/prisma");
-
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, "0.0.0.0", () => {
-  logger.info({ port: PORT }, "server listening");
-  console.log(`Server running on port ${PORT}`);
-});
-
-async function shutdown() {
-  await prisma.$disconnect();
-  server.close(() => process.exit(0));
-}
-
-process.on("SIGINT", shutdown);
-process.on("SIGTERM", shutdown); */
 const app = require("./app");
 const logger = require("./lib/logger");
 const prisma = require("./lib/prisma");
 
 const PORT = process.env.PORT || 3000;
 
-console.log("PORT =", PORT);
-
 const server = app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
   logger.info({ port: PORT }, "server listening");
-});
-
-server.on("error", (err) => {
-  console.error("SERVER ERROR:", err);
+  console.log(`Server running on port ${PORT}`);
 });
 
 async function shutdown() {
@@ -39,4 +15,5 @@ async function shutdown() {
 }
 
 process.on("SIGINT", shutdown);
-process.on("SIGTERM", shutdown);
+process.on("SIGTERM", shutdown); 
+
