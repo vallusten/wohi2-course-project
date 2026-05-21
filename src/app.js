ee0fc9cb513aa3d932a7aaf6e30d61f666d25b01
Valtteri Hermonen <164264621+vallusten@users.noreply.github.com>
2026-05-21 16:38:30 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const path = require("path");
+/*const path = require("path");
 const express = require("express");
 const pinoHttp = require("pino-http");
 const logger = require("./lib/logger");
@@ -30,4 +30,30 @@ app.use((err, req, res, next) => {
   });
 });
 
-module.exports = app;
+module.exports = app; */
+const logger = require("./lib/logger");
+console.log("1 logger loaded");
+
+const prisma = require("./lib/prisma");
+console.log("2 prisma loaded");
+
+console.log("3 loading app...");
+const app = require("./app");
+console.log("4 app loaded");
+
+const PORT = process.env.PORT || 3000;
+
+console.log("5 starting server");
+
+const server = app.listen(PORT, "0.0.0.0", () => {
+  console.log("6 SERVER LISTENING");
+  logger.info({ port: PORT }, "server listening");
+});
+
+async function shutdown() {
+  await prisma.$disconnect();
+  server.close(() => process.exit(0));
+}
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
