5786529cfdd4fec77142b2ddb00166303307690f
Valtteri Hermonen <164264621+vallusten@users.noreply.github.com>
2026-05-21 17:17:24 +0300

    Add keyword relation

---
+++ prisma/migrations/20260521141700_add_keyword_relation/migration.sql
@@ -0,0 +1,5 @@
+-- RenameIndex
+ALTER TABLE `_keywordtopost` RENAME INDEX `_KeywordToPost_AB_unique` TO `_keywordtopost_AB_unique`;
+
+-- RenameIndex
+ALTER TABLE `_keywordtopost` RENAME INDEX `_KeywordToPost_B_index` TO `_keywordtopost_B_index`;
